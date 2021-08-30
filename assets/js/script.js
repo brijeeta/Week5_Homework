fc777fd645782ca49633fb7b2a9a34a319304a27
Holy Paul <brijeeta@gmail.com>
2021-08-29 22:51:16 -0400

    added timeblocks elements in html using bootstrap and fontawesome, added basic functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
+var allHours = $('.time-block');
 var header = $('.jumbotron');
 var today = moment();
-$("#currentDay").text(today.format("MMM Do, YYYY"));
+var currentDay = $("#currentDay");
+
+//  set the current day on the header 
+currentDay.text(today.format("MMM Do, YYYY"));
 header.append(today);
+
+setBkgrndColor();
+
+// Clear Planner
+function clearStorage() {
+    localStorage.clear();
+    loadPlanner();
+}
+// Set background colors based on current time ( current- red, past-grey, future - green)
+function setBGColor() {}
+
+// Load local storage on load
+function loadPlanner() {};
